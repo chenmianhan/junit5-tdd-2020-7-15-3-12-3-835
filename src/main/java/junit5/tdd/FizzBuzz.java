359d6dda870020430e372a54deff8fe22544a847
JAMES M H CHEN (ITA-ISDC-ISD-OOCLL/ZHA) <CHENJA28@OOCL.COM>
2020-07-23 11:38:28 +0800

    [FizzBuzz][James Chen] feature: add should_return_1_when_play_fizz_buzz_when_given_1 test

---
+++ src/main/java/junit5/tdd/FizzBuzz.java
@@ -0,0 +1,7 @@
+package junit5.tdd;
+
+public class FizzBuzz {
+    String play(int number){
+        return null;
+    }
+}
---
+++ src/test/java/junit5/tdd/PlayFizzBuzzTest.java
@@ -0,0 +1,30 @@
+package junit5.tdd;
+
+import javafx.scene.media.AudioClip;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class PlayFizzBuzzTest {
+    @BeforeAll
+    static void initTestEnv() {
+    }
+
+    @BeforeEach
+    void initEveryMethod() {
+    }
+    @Test
+    void should_return_1_when_play_fizz_buzz_when_given_1(){
+        //given
+        FizzBuzz fizzBuzz=new FizzBuzz();
+        //when
+
+        String actual=fizzBuzz.play(1);
+        //then
+
+        assertThat(actual, is("1"));
+    }
+}
