473e213d6c80185646a2d3811e793e7c59df4a55
JAMES M H CHEN (ITA-ISDC-ISD-OOCLL/ZHA) <CHENJA28@OOCL.COM>
2020-07-23 11:59:59 +0800

    [FizzBuzz][James Chen] feature: implemented the should_return_fizzBuzz_when_play_fizz_buzz_when_given_15

--- src/main/java/junit5/tdd/FizzBuzz.java
+++ src/main/java/junit5/tdd/FizzBuzz.java
@@ -2,9 +2,11 @@ package junit5.tdd;
 
 public class FizzBuzz {
     String play(int number){
+        if(number%3==0&&number%5==0)return "FizzBuzz";
         if(number%3==0)return "Fizz";
         if(number%5==0)return "Buzz";
         if(number%7==0)return "Whizz";
+
         return String.valueOf(number);
     }
 }
